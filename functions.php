37fb1869ac541fe3a41ea992865f4f64259c6f58
mateuswetah <mateus.m.luna@gmail.com>
2020-07-07 15:38:22 -0300

    Tainacan compatible templates for TwentyTwenty.

---
+++ functions.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Twenty-twenty-child Theme functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package twenty-twenty-child
+ */
+
+add_action( 'wp_enqueue_scripts', 'twentytwenty_parent_theme_enqueue_styles' );
+
+/**
+ * Enqueue scripts and styles.
+ */
+function twentytwenty_parent_theme_enqueue_styles() {
+	wp_enqueue_style( 'twentytwenty-style', get_template_directory_uri() . '/style.css' );
+	wp_enqueue_style( 'twenty-twenty-child-style',
+		get_stylesheet_directory_uri() . '/style.css',
+		array( 'twentytwenty-style' )
+	);
+
+}
